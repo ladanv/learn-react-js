523fd9c5ce5be392962d8301194c9c5ce1766186
Vitaliy Ladan <vitaliy.ladan@gmail.com>
2016-10-14 20:40:22 +0600

    Changed routing

--- simpleapp/app/index.js
+++ simpleapp/app/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-import App from './component/App';
-import Home from './component/Home';
-import Warehouse from './component/Warehouse';
-import Settings from './component/Settings';
-import InventItemList from './component/InventItemList';
-import InventItem from './component/InventItem';
+import { Router, hashHistory } from 'react-router'
+import { routes } from './routes'
 
-
-render(
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={Home} />
-      <Route path='/inventory' component={InventItemList} />
-      <Route path='/inventory/:itemId' component={InventItem} />
-      <Route path='/warehouse' component={Warehouse} />
-      <Route path='/settings' component={Settings} />
-    </Route>
-  </Router>,
-  document.getElementById('root')
+render (
+    <Router history={hashHistory} routes={routes} />,
+    document.getElementById('root')
 )
