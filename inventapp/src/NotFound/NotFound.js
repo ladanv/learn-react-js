042047701f9fdb7e02aa7ae2ac3dc36aed1045cc
Vitaliy Ladan <vitaliyladan@count0.local>
2016-10-29 15:35:29 +0600

    Styled NotFound component

--- inventapp/src/NotFound/NotFound.js
+++ inventapp/src/NotFound/NotFound.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
+import style from './NotFound.scss';
 
 const NotFound = () => (
-  <p>Page not found</p>
+  <div className={style.container}>
+    <h2 className={style.title}>Page not found</h2>
+    <p className={style.message}>
+      The requested page is not found. Probably, the URL you are using is incorrect.
+    </p>
+    <Link to="/" className={style.backUrl}>Return back to home page</Link>
+  </div>
 );
 
 export default NotFound;
